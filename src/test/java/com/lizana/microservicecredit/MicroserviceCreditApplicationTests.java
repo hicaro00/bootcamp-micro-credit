8ef898dcc103098c0e2de3a281401eb80f7c7835
humberto <humberto.lizana@gmail.com>
2023-10-14 23:46:44 -0500

    first commit

---
+++ src/test/java/com/lizana/microservicecredit/MicroserviceCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizana.microservicecredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
