727cee571c1ab1ee28802c474ecd822e76b5e3f9
humberto <humberto.lizana@gmail.com>
2023-10-29 10:33:33 -0500

    fix

--- src/main/java/com/lizana/microservicecredit/MicroserviceCreditApplication.java
+++ src/main/java/com/lizana/microservicecredit/MicroserviceCreditApplication.java
@@ -1,11 +1,19 @@
 package com.lizana.microservicecredit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@AutoConfiguration
 public class MicroserviceCreditApplication {
 
+	@Bean
+	public WebClient webClient(){
+		return WebClient.builder().build();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceCreditApplication.class, args);
 	}
