6c0822cee7632353a1bb8dd26c58b09e585cf0f7
humberto <humberto.lizana@gmail.com>
2023-10-29 10:39:58 -0500

    inicio credit

--- src/main/java/com/lizana/microservicecredit/MicroserviceCreditApplication.java
+++ src/main/java/com/lizana/microservicecredit/MicroserviceCreditApplication.java
@@ -3,10 +3,12 @@ package com.lizana.microservicecredit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @AutoConfiguration
 public class MicroserviceCreditApplication {
 
